c7bde5529c995000516b6eca41a2c789f7f087a5
heqian <664516715@qq.com>
2018-11-13 11:29:02 +0800

    backup

---
+++ src/constant/link.js
@@ -0,0 +1,12 @@
+
+const BUY = 'https://weidian.com/item.html?itemID=2621089563&p=iphone&wfr=wxBuyerShare&share_relation=3288c65866e6f5c6_192567885_1'
+
+const PARTNERAPPLY = 'http://cn.mikecrm.com/zF12G1w'
+
+const DOWNLOAD_IOS = ''
+
+const DOWNLOAD_ANDRIOD = 'https://www.ifoodschain.com/download/android/suprobe-android-V1.0.6.apk'
+
+export default{
+  BUY, PARTNERAPPLY, DOWNLOAD_IOS, DOWNLOAD_ANDRIOD
+}
