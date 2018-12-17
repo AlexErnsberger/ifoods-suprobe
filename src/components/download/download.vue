d8c478285c6c21467560192a17338db860e0c8ea
heqian <664516715@qq.com>
2018-12-17 18:23:51 +0800

    homepage download add

---
+++ src/components/download/download.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="download-module">
+    <div class="download-title">
+      <slot name="title"></slot>
+    </div>
+    <div class="download-content">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  .download-module{
+    position: fixed;
+    z-index: 2;
+    display: inline-block;
+    width: 180px;
+    right:-80px;
+    transition: transform .5s linear;
+    .download-title, .download-content{
+      float: left;
+    }
+    .download-title{
+      background-color:  @common-color;
+      color: #fff;
+      text-align: center;
+      width: 100px;line-height: 80px;
+    }
+    &:hover{
+      transition: transform .5s linear;
+      transform: translateX(-80px);
+    }
+    .download-content{
+      text-align: center;
+    }
+  }
+</style>
--- src/views/SuprobeHome.vue
+++ src/views/SuprobeHome.vue
@@ -4,6 +4,14 @@
   <home-desc-pics></home-desc-pics>
   <home-news></home-news>
   <home-qr-code></home-qr-code>
+  <home-download class="ios-download">
+    <span slot="title">IOS 下载</span>
+    <img slot="content" src="/static/img/home/download-ios.png" width="80px">
+  </home-download>
+  <home-download class="andriod-download">
+    <span slot="title">Andriod 下载</span>
+    <img slot="content" src="/static/img/home/download-andriod.png" width="80px">
+  </home-download>
 </div>
 </template>
 
@@ -13,16 +21,24 @@ import HomeNews from '@/components/Home/HomeNews.vue'
 import HomeQrCode from '@/components/Home/HomeQrCode.vue'
 import HomeDescPics from '@/components/Home/HomeDescPics.vue'
 import classmix from '@/mixin/mixin-wow.js'
+import HomeDownload from '@/components/download/download.vue'
 export default {
   components: {
     HomeSwiper,
     HomeNews,
     HomeQrCode,
-    HomeDescPics
+    HomeDescPics,
+    HomeDownload
   },
   mixins: [classmix]
 }
 </script>
 
 <style lang="less" scoped>
+.ios-download{
+  top: 40%;
+}
+.andriod-download{
+  top: 52%;
+}
 </style>
